87a85f7e1dd502f173810f73f3055482bca01969
shaoshengsong <30892500+shaoshengsong@users.noreply.github.com>
2019-07-30 19:29:16 +0800

    Update run_web_return_json.py

--- run_web_return_json.py
+++ run_web_return_json.py
@@ -44,7 +44,8 @@ if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
 
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+#MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_NAME = 'ssd_mobilenet_v2_oid_v4_2018_12_12'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
@@ -52,8 +53,8 @@ DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-
+#PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join('data', 'oid_v4_label_map.pbtxt')
 
 opener = urllib.request.URLopener()
 opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
@@ -108,58 +109,75 @@ TEST_IMAGE_PATHS = [os.path.join(
 IMAGE_SIZE = (12, 8)
 
 
-def run_inference_for_single_image(image, graph):
-    with graph.as_default():
-        with tf.Session() as sess:
-            # Get handles to input and output tensors
-            ops = tf.get_default_graph().get_operations()
-            all_tensor_names = {
-                output.name for op in ops for output in op.outputs}
-            tensor_dict = {}
-            for key in [
-                'num_detections', 'detection_boxes', 'detection_scores',
-                'detection_classes', 'detection_masks'
-            ]:
-                tensor_name = key + ':0'
-                if tensor_name in all_tensor_names:
-                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-                        tensor_name)
-            if 'detection_masks' in tensor_dict:
-                # The following processing is only for single image
-                detection_boxes = tf.squeeze(
-                    tensor_dict['detection_boxes'], [0])
-                detection_masks = tf.squeeze(
-                    tensor_dict['detection_masks'], [0])
-                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                real_num_detection = tf.cast(
-                    tensor_dict['num_detections'][0], tf.int32)
-                detection_boxes = tf.slice(detection_boxes, [0, 0], [
-                                           real_num_detection, -1])
-                detection_masks = tf.slice(detection_masks, [0, 0, 0], [
-                                           real_num_detection, -1, -1])
-                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                    detection_masks, detection_boxes, image.shape[1], image.shape[2])
-                detection_masks_reframed = tf.cast(
-                    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                # Follow the convention by adding back the batch dimension
-                tensor_dict['detection_masks'] = tf.expand_dims(
-                    detection_masks_reframed, 0)
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-            # Run inference
-            output_dict = sess.run(tensor_dict,
-                                   feed_dict={image_tensor: image})
+# added to put object in JSON
+class Object(object):
+    def __init__(self):
+        self.name=" TensorFlow Object Detection  API"
 
-            # all outputs are float32 numpy arrays, so convert types as appropriate
-            output_dict['num_detections'] = int(
-                output_dict['num_detections'][0])
-            output_dict['detection_classes'] = output_dict[
-                'detection_classes'][0].astype(np.int64)
-            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-            output_dict['detection_scores'] = output_dict['detection_scores'][0]
-            if 'detection_masks' in output_dict:
-                output_dict['detection_masks'] = output_dict['detection_masks'][0]
-    return output_dict
+    def toJSON(self):
+        return json.dumps(self.__dict__)
+
+def get_objects(image, threshold=0.5):
+   # with graph.as_default():
+   #   with tf.Session() as sess:
+    with detection_graph.as_default():
+     with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+
+
+        image_np = load_image_into_numpy_array(image)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
+        boxes = np.squeeze(boxes)
+
+        obj_above_thresh = sum(n > threshold for n in scores)
+        print("detected %s objects in image above a %s score" % (obj_above_thresh, threshold))
+
+        output = []
+
+        # Add some metadata to the output
+        item = Object()
+        item.version = "0.0.1"
+        item.numObjects = int(obj_above_thresh)
+        item.threshold = threshold
+        output.append(item)
+
+        for c in range(0, len(classes)):
+            class_name = category_index[classes[c]]['name']
+            if scores[c] >= threshold:      # only return confidences equal or greater than the threshold
+                print(" object %s - score: %s, coordinates: %s" % (class_name, scores[c], boxes[c]))
+
+                item = Object()
+                item.name = 'Object'
+                item.class_name = class_name
+                item.score = float(scores[c])
+                item.y = float(boxes[c][0])
+                item.x = float(boxes[c][1])
+                item.height = float(boxes[c][2])
+                item.width = float(boxes[c][3])
+
+                output.append(item)
+
+        outputJson = json.dumps([ob.__dict__ for ob in output])
+        print(outputJson)
+        return outputJson
 
 #--------------------------------------------------------------------------------------------------------------------------------
 app = Flask(__name__, static_folder="web_static")
@@ -215,27 +233,9 @@ def get_feature_from_client(request_filename):
 @app.route("/predict", methods=["POST"])
 def predict():
     image1= get_feature_from_client('pic1')
-    image_np = load_image_into_numpy_array(image1)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(
-        image_np_expanded, detection_graph)
-    # Visualization of the results of a detection.
-    image=vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    byte_io = BytesIO()
-    array2image(image).save(byte_io, 'JPEG')
-    byte_io.seek(0)
+    json_str=get_objects(image1)
 
-    return send_file(byte_io, mimetype='image/jpeg')
+    return json_str
 
 def image2array(image):
     (w, h) = image.size
